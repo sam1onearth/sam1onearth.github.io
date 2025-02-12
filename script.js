ef5423286110434f52b2d1572131bec1ab4dcf01
sam1onearth <samuel@greenspinach.com>
2025-02-12 10:38:44 +0100

    Qdd vitql files or else deqth of project
    Add basic files

---
+++ script.js
@@ -0,0 +1,24 @@
+function maFonction()
+{
+    alert("Le JavaScript fonctionne !")
+    }
+    
+function foncRouge() 
+    {
+        document.querySelector(".para").classList.remove("vert");
+        document.querySelector(".para").classList.add("rouge");
+        }
+        
+function foncVert() 
+        {
+        document.querySelector(".para").classList.remove("rouge");
+        document.querySelector(".para").classList.add("vert");
+        } 
+        
+function modifMessage() {
+            document.querySelector(".para").innerHTML = "Bravo, vous avez cliqué sur le bouton !"
+            }
+
+
+let x ="5"+1+2;
+print(x)
